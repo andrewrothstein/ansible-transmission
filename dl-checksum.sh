9a23027e982fc77922b1155fb0fc1a372025b756
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-12-11 03:34:52 +0000

    transmission:4.0.5, cmake based build

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -19,8 +19,4 @@ dl_ver()
     printf "  '%s': sha256:%s\n" $ver `sha256sum $lfile | awk '{print $1}'`
 }
 
-dl_ver 4.0.0
-dl_ver 4.0.1
-dl_ver 4.0.2
-dl_ver 4.0.3
-dl_ver ${1:-4.0.4}
+dl_ver ${1:-4.0.5}
